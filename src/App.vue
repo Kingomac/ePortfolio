186b86742679877c0dcb9d590e1f1defdf768c6c
Kingomac <31375644+Kingomac@users.noreply.github.com>
2019-09-19 21:54:59 +0200

    Cambio a Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,69 @@
 <template>
-  <div id="app">
-    <div class="row">
-      <div class="col s12 m3 l2 red darken-1" id="sidenav-col"><SideMenu/></div>
-      <div class="col s12 m9 l10"><div class="container"><router-view/></div></div>
-    </div>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>mdi-view-dashboard</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Dashboard</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <v-list-item @click="">
+          <v-list-item-action>
+            <v-icon>mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>Settings</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar
+      app
+      clipped-left
+    >
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>Application</v-toolbar-title>
+    </v-app-bar>
+
+    <v-content>
+        <v-row
+          align="top"
+          justify="center"
+        >
+        <v-container
+        class="fill-height px-8 py-6"
+        fluid
+      >
+        <router-view/>
+        </v-container>
+        </v-row>
+      
+    </v-content>
+
+    <v-footer app>
+      <span>&copy; 2019</span>
+    </v-footer>
+  </v-app>
 </template>
 
 <script>
-import Header from './components/Header.vue'
-import SideMenu from './components/SideMenu.vue'
-
-export default {
-  name: 'app',
-  components: {
-    Header,
-    SideMenu
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+    }),
+    created () {
+      this.$vuetify.theme.dark = true
+    },
   }
-}
 </script>
-
-<style>
-#sidenav-col{
-  margin: 0px !important;
-  padding: 0px !important;
-  min-height: 100vh;
-}
-.row{ 
-  margin: 0px;
-}
-</style>
